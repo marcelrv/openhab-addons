952fa43aa4fb90a080f8ed8213220e994f9e3916
Marcel Verpaalen <marcel@verpaalen.com>
2021-12-21 12:40:28 +0100

    [philipsTV] update headers etc to allow maven compile
    Also updates pom to fix issue in eclipse & version
    
    Signed-off-by: Marcel Verpaalen <marcel@verpaalen.com>

--- bundles/org.openhab.binding.philipstv/src/main/java/org/openhab/binding/philipstv/internal/service/model/ambilight/AmbilightTopologyDto.java
+++ bundles/org.openhab.binding.philipstv/src/main/java/org/openhab/binding/philipstv/internal/service/model/ambilight/AmbilightTopologyDto.java
@@ -1,13 +1,13 @@
 /**
- * Copyright (c) 2010-2020 Contributors to the openHAB project
- * <p>
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
  * See the NOTICE file(s) distributed with this work for additional
  * information.
- * <p>
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0
- * <p>
+ *
  * SPDX-License-Identifier: EPL-2.0
  */
 package org.openhab.binding.philipstv.internal.service.model.ambilight;
@@ -18,17 +18,17 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 /**
  * The {@link AmbilightTopologyDto} class defines the Data Transfer Object
  * for the Philips TV API /ambilight/topology endpoint to retrieve the ambilight topology information.
- * <p>
+ *
  * Endpoint returns:
- * <p>
+ *
  * layers (integer): The number of layers.
- * <p>
+ *
  * left (integer): The number of pixels on the left.
- * <p>
+ *
  * top (integer): The number of pixels on the top.
- * <p>
+ *
  * right (integer): The number of pixels on the right.
- * <p>
+ *
  * bottom (integer): The number of pixels on the bottom.
  *
  * @author Benjamin Meyer - Initial contribution
